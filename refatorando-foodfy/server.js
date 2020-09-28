2b362fa3a4ee866533e2978c5d17a2bb49adbbcc
José Ulombe <jose.ulombe@hotmail.com>
2020-09-28 18:37:05 +0100

    initial server

---
+++ refatorando-foodfy/server.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+
+//starting server
+
+server.listen(3000, function() {
+    console.log("server is running")
+})
