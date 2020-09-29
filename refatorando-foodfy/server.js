06bcbf4c9416f70b1c4d3dedbedc0780417b51a4
José Ulombe <jose.ulombe@hotmail.com>
2020-09-29 18:39:41 +0100

    Update server

--- refatorando-foodfy/server.js
+++ refatorando-foodfy/server.js
@@ -1,63 +1,5 @@
-const express = require('express')    //importando a biblioteca do express
-const nunjucks = require('nunjucks')  //importando a biblioteca do nunjucks
 
-const server = express()
-const recipes = require("./data") //calling array
-
-//chamando o css
-server.use(express.static('public'))
-
-//configure template engine(nunjucks)
-
-server.set("view engine", "njk")   //setar qual é o motor de views da app, qual é a extensão dos arquivos para abrir 
-nunjucks.configure("views", {
-    express: server,               //indica ao nunjucks que vamos usar o Express com a var Server
-    autoescape: false,             //impede que o nunjucks mostre o codigo html em variaveis
-    noCache: true                  //bloqueando o cache do nunjucks         
-    
-})
-
-//creating routes
-server.get("/", function(req, res) {
-    return res.render("home")
-})
-
-
-server.get("/recipes", function(req, res) {
-    
-    return res.render("recipes", {about})
-})
-
-
-
-
-  //adicionando rota da página de receita
-  server.get("/recipes/:index", function(req, res) {
-    const id = req.params.id
-    
-    return res.render("course", { item: course })
-})
-
-
-
-server.use(function(req, res) {
-    res.status(404).render("not-found");
-  });
-
-
-
-server.listen(3000, function() {
-    console.log("server is running")
-})
-
-
-
-
-
-
-
-
-/*const express = require('express')
+const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
@@ -67,7 +9,7 @@ const receitas = require("./data")
 server.use(express.static('public'))
 
 //configure template engine
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express: server
@@ -93,15 +35,79 @@ server.get("/receitas", function(req, res) {
 
 
 
-
-
-
-
-
-
-
-
 //starting server
+
+server.listen(3000, function() {
+    console.log("server is running")
+})
+
+
+
+
+
+/*const express = require('express')    //importando a biblioteca do express
+const nunjucks = require('nunjucks')  //importando a biblioteca do nunjucks
+
+const server = express()
+const recipes = require("./data") //calling array
+
+//chamando o css
+server.use(express.static('public'))
+
+//configure template engine(nunjucks)
+
+server.set("view engine", "njk")   //setar qual é o motor de views da app, qual é a extensão dos arquivos para abrir 
+nunjucks.configure("views", {
+    express: server,               //indica ao nunjucks que vamos usar o Express com a var Server
+    autoescape: false,             //impede que o nunjucks mostre o codigo html em variaveis
+    noCache: true                  //bloqueando o cache do nunjucks         
+    
+})
+
+//creating routes
+server.get("/", function(req, res) {
+
+    let recipesFiltered = []
+
+    for(let i = 0; i < 6; i++) {
+        recipesFiltered.push(recipes[i])
+    }
+
+    console.table(recipesFiltered)
+
+    return res.render("home", { items: recipes })
+})
+
+
+server.get("/recipes", function(req, res) {
+    
+    return res.render("recipes", { items: recipes })
+})
+
+
+
+
+  //adicionando rota da página de receita
+  server.get("/recipes/:index", function(req, res) {
+      const { index: recipeIndex } = req.params
+
+
+      const recipe = recipes [recipeIndex]
+
+      if (!recipe) return res.send("Recipe not found!")
+
+      console.log(recipe)
+
+      return res.render("recipe", { item: recipe })
+})
+
+
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+
+
 
 server.listen(3000, function() {
     console.log("server is running")
